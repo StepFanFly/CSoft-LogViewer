c0743d4ea7793eb2332c678a1e074d333426ad75
Anton <tosha37@inbox.ru>
2021-01-18 12:43:04 +0300

    Init

---
+++ LogViewer/Infrastructure/DialogService.cs
@@ -0,0 +1,64 @@
+﻿using LogViewer.DialogWindows;
+using MaterialDesignThemes.Wpf;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Threading.Tasks;
+using CommonServiceLocator;
+using LogViewer.Models;
+
+namespace LogViewer
+{
+    public interface IDialogService
+    {
+        void Show(object content);
+
+        Task<bool> ShowAsync(object content);
+    }
+
+    public class DialogService : IDialogService
+    {
+        public void Show(object content)
+        {
+            if (content is ObservableCollection<LogFile> contentWithLogFiles) 
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Multiselect = true;
+                dialog.Filter = "Log files (*.log)|*.log|All files (*.*)|*.*";
+                if (true == dialog.ShowDialog())
+                {
+                    foreach (string filename in dialog.FileNames) 
+                    {
+                        var currFile = new FileInfo(filename);
+                        if (currFile.Exists) 
+                        {
+                            contentWithLogFiles.Add(new LogFile { FileInfo = currFile, FilePath = filename });
+                        }
+                    }
+                }
+            }
+        }
+
+        public async Task<bool> ShowAsync(object content)
+        {
+            bool bRes = false;
+
+            if (content is ObservableCollection<Filter> filters)
+            {
+                ServiceLocator.Current.GetInstance<FilterVM>().AddFilters(filters);
+                var view = new FilterDialog();
+                //show the dialog
+                var result = await DialogHost.Show(view, "RootDialog", null, (sender, eventArgs) => { bRes = (bool)eventArgs.Parameter; });
+            }
+
+            if (content is Filter) 
+            {
+                var view = new FilterTypeDialog();
+                //show the dialog
+                var result = await DialogHost.Show(view, "FilterDialog", null, (sender, eventArgs)=> { bRes = (bool)eventArgs.Parameter; });
+            }
+
+            return bRes;
+        }
+    }
+}
---
+++ LogViewer/Infrastructure/LogFileParser.cs
@@ -0,0 +1,40 @@
+﻿using LogViewer.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LogViewer
+{
+    interface Iparser<T> where T : LogFile
+    {
+        T GetLogFile();
+        void ApplyAllFilters( IEnumerable<Filter> filters);
+        string GetResult();
+    }
+
+    class LogFileParser : Iparser<LogFile>
+    {
+        public LogFileParser(LogFile file)
+        {
+            _file = file;
+        }
+
+        public void ApplyAllFilters( IEnumerable<Filter> filters)
+        {
+            foreach (var filter in filters)
+                _parseString = filter.Apply(_file);
+        }
+
+        public LogFile GetLogFile()
+        {
+            return _file;
+        }
+
+        public string GetResult()
+        {
+            return _parseString;
+        }
+
+        LogFile _file;
+        string _parseString = new string("");
+    }
+}
