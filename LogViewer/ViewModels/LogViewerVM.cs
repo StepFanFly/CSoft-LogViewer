ded937a36966ba2080ef20502d9ff98f94adde06
Anton <tosha37@inbox.ru>
2021-01-19 00:21:16 +0300

    Added exit application command, fix selection binding in combo box

--- LogViewer/ViewModels/LogViewerVM.cs
+++ LogViewer/ViewModels/LogViewerVM.cs
@@ -4,53 +4,70 @@ using GalaSoft.MvvmLight.Command;
 using LogViewer.Models;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Windows;
 
 namespace LogViewer
 {
-    public class LogViewerVM : ViewModelBase 
+    public class LogViewerVM : ViewModelBase
     {
         public ObservableCollection<LogFile> LogFiles { get; set; } = new ObservableCollection<LogFile>() {  /*new LogFile { FileInfo = new FileInfo("Test"), FilePath ="sdfsdfsd"}*/ };
 
         public ObservableCollection<Filter> Filters { get; set; } = new ObservableCollection<Filter>() {/* new FileNameFilter { }, new FileNameFilter { }, new FileNameFilter { }, new FileNameFilter { } */};
 
-        private readonly IDialogService _dlgService = ServiceLocator.Current.GetInstance<IDialogService>();
-
-        private readonly Iparser<LogFile> Parser; 
-
-        public LogFile SelLogFile { get; set; }
-
+         public LogFile SelLogFile { get; set; }
 
         public string ViewSting { get; set; }
-        
+
         public LogViewerVM()
         {
-            LogFiles = new ObservableCollection<LogFile>(){  new LogFile { FileInfo = new FileInfo("Super"), FilePath ="sdfsdfsd"}};
+            LogFiles = new ObservableCollection<LogFile>() { new LogFile { FileInfo = new FileInfo("Super"), FilePath = "sdfsdfsd" } };
             Parser = new LogFileParser(SelLogFile);
         }
 
-        public RelayCommand OpenCommand => new RelayCommand(()=> { _dlgService.Show(LogFiles); });
+        public RelayCommand OpenCommand => new RelayCommand(() => { _dlgService.Show(LogFiles); });
 
-        public RelayCommand OpenFilterDlgCommand => new RelayCommand(async  () => 
+        public RelayCommand OpenFilterDlgCommand => new RelayCommand(async () =>
         {
             var dlgRes = await _dlgService.ShowAsync(Filters);
 
-            if (dlgRes == true) 
+            if (dlgRes == true)
             {
                 Filters = ServiceLocator.Current.GetInstance<FilterVM>().Filters;
                 Parser.ApplyAllFilters(Filters);
                 ViewSting = Parser.GetResult();
             }
+        }, () => { return SelLogFile != null; });
+
+        public RelayCommand<LogFile> RemoveSelLogCommand => new RelayCommand<LogFile>(logFileToRemove => LogFiles.Remove(logFileToRemove));
+
+        public RelayCommand<Filter> RemoveFilterCommand => new RelayCommand<Filter>((filter) =>
+        {
+            Filters.Remove(filter);
         });
 
-        public RelayCommand<LogFile> RemoveSelLogCommand => new RelayCommand<LogFile>(logFileToRemove => LogFiles.Remove(logFileToRemove) );
-
-        public RelayCommand<LogFile> ShowLogCommand { get { return new RelayCommand<LogFile>(async(selFile)=> 
+        public RelayCommand<LogFile> ShowLogCommand
         {
-            if (selFile != null) 
+            get
             {
+                return new RelayCommand<LogFile>(async (selFile) =>
+                {
+                if (selFile != null)
+                {
                 await selFile.ReadFileContentAsync();
                 ViewSting = selFile.Content;
+                }
+                });
             }
-        }); } }
+        }
+
+
+        public RelayCommand ExitApplicationCommand  => new RelayCommand(() =>
+        {
+            if (Application.Current.MainWindow != null) Application.Current.MainWindow.Close();
+        });
+
+        private readonly IDialogService _dlgService = ServiceLocator.Current.GetInstance<IDialogService>();
+
+        private readonly Iparser<LogFile> Parser;
     }
 }
