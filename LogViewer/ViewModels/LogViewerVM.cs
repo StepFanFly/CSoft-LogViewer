29ce0858593d9786a417ac37863ce43dc49eead7
Anton <tosha37@inbox.ru>
2021-01-18 13:08:41 +0300

    Test

--- LogViewer/ViewModels/LogViewerVM.cs
+++ LogViewer/ViewModels/LogViewerVM.cs
@@ -9,6 +9,7 @@ namespace LogViewer
 {
     public class LogViewerVM : ViewModelBase 
     {
+        //files
         public ObservableCollection<LogFile> LogFiles { get; set; } = new ObservableCollection<LogFile>() {  /*new LogFile { FileInfo = new FileInfo("Test"), FilePath ="sdfsdfsd"}*/ };
 
         public ObservableCollection<Filter> Filters { get; set; } = new ObservableCollection<Filter>() {/* new FileNameFilter { }, new FileNameFilter { }, new FileNameFilter { }, new FileNameFilter { } */};
@@ -20,7 +21,7 @@ namespace LogViewer
         public LogFile SelLogFile { get; set; }
 
         public string ViewSting { get; set; }
-
+        
         public LogViewerVM()
         {
             LogFiles = new ObservableCollection<LogFile>(){  new LogFile { FileInfo = new FileInfo("Super"), FilePath ="sdfsdfsd"}};
