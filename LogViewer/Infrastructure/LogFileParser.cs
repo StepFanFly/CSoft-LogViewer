a79d182bcd3738b5f7d737f91365572f8e6a1795
Oum Eoshi <zsm91@mail.ru>
2021-01-21 17:36:32 +0300

    Added basic operations in mergin filter

--- LogViewer/Infrastructure/LogFileParser.cs
+++ LogViewer/Infrastructure/LogFileParser.cs
@@ -1,6 +1,7 @@
 ﻿using LogViewer.Models;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace LogViewer
 {
@@ -21,8 +22,45 @@ namespace LogViewer
 
         public void ApplyAllFilters( IEnumerable<Filter> filters)
         {
-            foreach (var filter in filters)
-                _parseString = filter.Apply(_file);
+            _parseString = "";
+            //for each filter we call for apply, get dictionary in result
+            //then collapse alldictionaries
+            //or just calculate result on go
+            if (filters.Count() > 0) {
+                Dictionary<int, string> result = new Dictionary<int, string>();
+                eOperationType storedOperation = eOperationType.eUnion;
+                foreach (var filter in filters)
+                {
+                    Dictionary<int, string> tmp = new Dictionary<int, string>();
+                    tmp = filter.ApplyFilter();
+                    switch (storedOperation)
+                    {
+                        case eOperationType.eUnion:
+                            tmp.ToList().ForEach(x => result[x.Key] = x.Value);
+                            break;
+                        case eOperationType.eIntersect:
+                            Dictionary<int, string> dict = new Dictionary<int, string>();
+                            dict = result.Where(x => tmp.ContainsKey(x.Key)).ToDictionary(x => x.Key, x=>x.Value);
+                            result = dict;
+                            break;
+                        case eOperationType.eShielding:
+                            //remove from result all pairs from tmp somehow
+                            //dict = result.Where(x => tmp.ContainsKey(x.Key)).ToDictionary(x => x.Key, x => x.Value);
+                            //result = dict;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    storedOperation = filter.SelectedOperationType;
+                }
+                foreach (var pair in result)
+                    _parseString += pair.Value + "\n";
+            }
+            else
+            {
+                _parseString = _file.Content;
+            }
         }
 
         public LogFile GetLogFile()
